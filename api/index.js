10e7a4fe9337f7c67d271921a5a7c4d67acd0d21
arturk22 <alencar.artur00@gmail.com>
2022-04-20 10:28:44 -0300

    finalizando api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,79 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const config = require('config')
+const roteador = require('express').Router()
+const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
+const SerializadorFornecedor = require('../../Serializador').SerializadorFornecedor
 
-app.use(bodyParser.json())
+roteador.get('/', async (requisicao, resposta) => {
+    const resultados = await TabelaFornecedor.listar()
+    resposta.status(200)
+    const serializador = new SerializadorFornecedor(
+        resposta.getHeader('Content-Type')
+    )
+    resposta.send(
+        serializador.serializar(resultados)
+    )
+})
 
-const roteador = require('./rotas/fornecedores')
-app.use('/api/fornecedores', roteador)
+roteador.post('/', async (requisicao, resposta, proximo) => {
+    try {
+        const dadosRecebidos = requisicao.body
+        const fornecedor = new Fornecedor(dadosRecebidos)
+        await fornecedor.criar()
+        resposta.status(201)
+        const serializador = new SerializadorFornecedor(
+            resposta.getHeader('Content-Type')
+        )
+        resposta.send(
+            serializador.serializar(fornecedor)
+        )
+    } catch (erro) {
+        proximo(erro)
+    }
+})
 
-app.listen(config.get('api.porta'), () => console.log('A API está funcionando!'))
+roteador.get('/:idFornecedor', async (requisicao, resposta, proximo) => {
+    try {
+        const id = requisicao.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        resposta.status(200)
+        const serializador = new SerializadorFornecedor(
+            resposta.getHeader('Content-Type'),
+            ['email', 'dataCriacao', 'dataAtualizacao', 'versao']
+        )
+        resposta.send(
+            serializador.serializar(fornecedor)
+        )
+    } catch (erro) {
+        proximo(erro)
+    }
+})
+
+roteador.put('/:idFornecedor', async (requisicao, resposta, proximo) => {
+    try {
+        const id = requisicao.params.idFornecedor
+        const dadosRecebidos = requisicao.body
+        const dados = Object.assign({}, dadosRecebidos, { id: id })
+        const fornecedor = new Fornecedor(dados)
+        await fornecedor.atualizar()
+        resposta.status(204)
+        resposta.end()
+    } catch (erro) {
+        proximo(erro)
+    }
+})
+
+roteador.delete('/:idFornecedor', async (requisicao, resposta, proximo) => {
+    try {
+        const id = requisicao.params.idFornecedor
+        const fornecedor = new Fornecedor({ id: id })
+        await fornecedor.carregar()
+        await fornecedor.remover()
+        resposta.status(204)
+        resposta.end()
+    } catch (erro) {
+        proximo(erro)
+    }
+})
+
+module.exports = roteador
